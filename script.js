94fc7d2baa29c672c668b2fa67449ad415ac5331
JavaAppa <javaappa05@gmail.com>
2025-08-31 15:41:21 -0700

    Started a list of cluster types. Added entity cluster. Changed .arg class to .cluster to better fit usage.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 function id(id) { return document.getElementById(id); }
+
+const clusterTypes = [
+    {
+        "styleTag": "vector",
+        "name": "Vector"
+    },
+    {
+        "styleTag": "scalar",
+        "name": "Scalar"
+    },
+    {
+        "styleTag": "entity",
+        "name": "Entity"
+    },
+    {
+        "styleTag": "null",
+        "name": "Null"
+    }
+];
