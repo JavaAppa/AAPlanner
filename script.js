e19bf4008385e9a9c3d992f7410c016127e2a5b4
JavaAppa <javaappa05@gmail.com>
2025-08-31 17:27:43 -0700

    Added styles for all the cluster types

--- script.js
+++ script.js
@@ -2,16 +2,36 @@ function id(id) { return document.getElementById(id); }
 
 const clusterTypes = [
     {
+        "styleTag": "entity",
+        "name": "Entity"
+    },
+    {
+        "styleTag": "block",
+        "name": "Block"
+    },
+    {
         "styleTag": "vector",
         "name": "Vector"
     },
     {
-        "styleTag": "scalar",
-        "name": "Scalar"
+        "styleTag": "item",
+        "name": "Item"
     },
     {
-        "styleTag": "entity",
-        "name": "Entity"
+        "styleTag": "rotation",
+        "name": "Rotation"
+    },
+    {
+        "styleTag": "word",
+        "name": "Word"
+    },
+    {
+        "styleTag": "boolean",
+        "name": "Boolean"
+    },
+    {
+        "styleTag": "scalar",
+        "name": "Scalar"
     },
     {
         "styleTag": "null",
